21013925e193881b623e39ca893a57334717c4ef
Nicolas Molina <nicolasjmolina1@gmail.com>
2023-11-27 11:45:24 -0300

    Initial Commit

---
+++ CoffeeUI/CoffeeUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeUIApp.swift
+//  CoffeeUI
+//
+//  Created by Nico Molina on 27/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
